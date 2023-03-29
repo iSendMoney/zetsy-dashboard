9997aac31fbe23461df2c916f55a7701c7953a2f
rpc404 <theboringschool.org@gmail.com>
2023-03-30 01:10:09 +0545

    Added Charts

--- src/components/Sidebar/SidebarApp.jsx
+++ src/components/Sidebar/SidebarApp.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import moment from "moment";
+import React from "react";
 
-import './styles/style.css'
+import OrderAnalyticsCard from "./OrderAnalyticsCard";
+import AnalyticsNumbersData from "../../mocks/analyticsNumbers.json";
+
+import "./styles/style.css";
 
 export default function SidebarApp() {
   return (
-    <div className='sidebarApp__container'>SidebarApp</div>
-  )
+    <div className="sidebarApp__container">
+      <div className="header">
+        <div className="greetings">
+          <h2>Good Evening, Jessica Singh Shrestha</h2>
+          <p>Here's your stat's for {moment().format("LL")}</p>
+        </div>
+        <div className="redirectToSite">
+          <p>Go to your site</p> <i className="ri-arrow-right-up-line"></i>
+        </div>
+      </div>
+      <div className="orderAnalytics">
+        {AnalyticsNumbersData.map((data, key) => (
+          <OrderAnalyticsCard
+            key={key}
+            data={data}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
