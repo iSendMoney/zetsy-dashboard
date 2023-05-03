5ee3a3141cea86a9b5c0758900f3a229e7df3717
rpc404 <theboringschool.org@gmail.com>
2023-05-03 23:14:12 +0545

    Completed Kanban & Calendar

--- src/pages/DashboardContainer/Calendar.jsx
+++ src/pages/DashboardContainer/Calendar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import FullCalendar from "@fullcalendar/react"; // must go before plugins
 import dayGridPlugin from "@fullcalendar/daygrid";
-import './styles/calendar.styles.css'
+import "./styles/calendar.styles.css";
 
 export default function Calendar() {
   return (
     <div className="calendar__container">
-      <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView="dayGridMonth"
+        events={[{ title: "My Anniversary", date: "2023-05-26" }]}
+      />
     </div>
   );
 }
